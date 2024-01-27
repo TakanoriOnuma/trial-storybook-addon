59d361690eed28e684380e824835c384f2e2b316
大沼 峻徳 <numa.san.1024@gmail.com>
2024-01-27 10:41:21 +0900

    storybookを立ち上げられるようにした

---
+++ README.md
@@ -0,0 +1,1 @@
+# storybook addonの検証
---
+++ stories/TestMessage.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { TestMessage } from "./TestMessage";
+
+type Story = StoryObj<typeof TestMessage>;
+
+const meta: Meta<typeof TestMessage> = {
+  title: "TestMessage",
+  component: TestMessage,
+  args: {
+    message: "Hello World",
+  },
+};
+
+export default meta;
+
+export const Default: Story = {};
