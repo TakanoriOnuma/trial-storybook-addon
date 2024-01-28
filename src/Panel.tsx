c3e7340e0f4c107af9d8333e4e6c37892e7317fb
大沼 峻徳 <numa.san.1024@gmail.com>
2024-01-28 11:45:50 +0900

    パラメータのリセットタイミングをStoryを切り替えた時に変えた

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -1,26 +1,38 @@
 import { Addon_BaseType } from "@storybook/types";
 import { AddonPanel } from "@storybook/components";
 import { PureArgsTable } from "@storybook/blocks";
-import { useParameter } from "@storybook/manager-api";
+import { useParameter, useStorybookState } from "@storybook/manager-api";
 import { PARAM_KEY } from "./constants";
 
 import { useEffect } from "react";
 import { useMyAddonStateFromManager } from "./myAddonState/useMyAddonStateFromManager";
 import { INITIAL_MY_ADDON_STATE } from "./myAddonState/MyAddonState";
 
+// この辺のコードを参考にパラメータを設定するコンポーネントを作った
+// https://github.com/storybookjs/storybook/blob/v7.6.10/code/addons/controls/src/ControlsPanel.tsx
+
 export const Panel: Addon_BaseType["render"] = ({ active }) => {
   const [myAddonState, setMyAddonState] = useMyAddonStateFromManager();
 
   const params = useParameter(PARAM_KEY, INITIAL_MY_ADDON_STATE);
+  const { path, previewInitialized } = useStorybookState();
 
+  // pathが変わった時にパラメータをリセットする
   useEffect(() => {
+    // まだpreviewが初期化されていない場合はスキップする
+    if (!previewInitialized) {
+      return;
+    }
     setMyAddonState(params);
-  }, [params]);
+  }, [path]);
 
   return (
     <AddonPanel active={active ?? false}>
       <h2>パラメータ設定</h2>
       <PureArgsTable
+        // みているStoryが変わったら作り直す
+        key={path}
+        isLoading={!previewInitialized}
         args={myAddonState}
         rows={{
           flag: {
